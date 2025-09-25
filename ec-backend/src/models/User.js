7cc3290a7a1f12dd6edf228bdc15f8ba2e98ef5b
jeanSebastienRakotonirina <jsr029.applyforjob@gmail.com>
2025-09-25 20:14:56 +0200

    u

--- ec-backend/src/models/User.js
+++ ec-backend/src/models/User.js
@@ -1,11 +1,11 @@
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  resetToken: { type: String },
-  resetTokenExpiry: { type: Date },
+  isAdmin: { type: Boolean, default: false },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
